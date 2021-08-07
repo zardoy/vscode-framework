5aae305718285dd514914a5ab0bb4936360b9511
Vitaly Turovsky <vital2580@icloud.com>
2021-08-07 21:32:19 +0300

    add descriptions

--- build/client.d.ts
+++ build/client.d.ts
@@ -1,8 +1,12 @@
 declare namespace NodeJS {
     interface ProcessEnv {
+        /** De-facto standard. Similar to ctx.extensionMode */
         NODE_ENV: "development" | "production";
+        /** `name` of package.json */
         EXTENSION_ID_NAME: string;
+        /** `displayName` of package.json */
         EXTENSION_DISPLAY_NAME: string;
+        /** Set to "false" to not reveal output pannel (on development only) */
         REVEAL_OUTPUT_PANEL_IN_DEVELOPMENT: string;
     }
 }
