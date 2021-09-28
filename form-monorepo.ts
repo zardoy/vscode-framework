661b9a6d22e3ef62a5747e3a32559fa7fd3ae400
Vitaly Turovsky <vital2580@icloud.com>
2021-09-29 01:43:12 +0300

    gen readme with packages

--- form-monorepo.ts
+++ form-monorepo.ts
@@ -1,8 +1,9 @@
 import fs from 'fs'
 import jsonfile from 'jsonfile'
-import { modifyTsConfigJsonFile } from 'modify-json-file'
+import { modifyPackageJsonFile, modifyTsConfigJsonFile } from 'modify-json-file'
 import { join } from 'path'
 import { PackageJson, TsConfigJson } from 'type-fest'
+import { getGithubRemoteInfo } from 'github-remote-info'
 ;(async () => {
     const fromMonorepo = (...p: string[]) => join(__dirname, 'packages', ...p)
     const packageTsconfigs = {
@@ -13,7 +14,7 @@ import { PackageJson, TsConfigJson } from 'type-fest'
         const fromPackage = (...p: string[]) => join(__dirname, 'packages', monorepoPackage, ...p)
         if (!fs.existsSync(fromPackage('package.json'))) return false
         const packageJson: PackageJson = jsonfile.readFileSync(fromPackage('package.json'))
-        if (!packageJson.types) return false
+        if (packageJson.private || !packageJson.types) return false
         return true
     })
     console.log(packagesDirs)
@@ -63,4 +64,25 @@ import { PackageJson, TsConfigJson } from 'type-fest'
     await modifyTsConfigJsonFile(join(__dirname, 'tsconfig.prod.json'), {
         references: packagesDirs.map(dir => ({ path: `packages/${dir}/${packageTsconfigs.prod}` })),
     })
+
+    const readme = await fs.promises.readFile('./README.MD', 'utf-8')
+
+    const { owner, name } = (await getGithubRemoteInfo('.'))!
+    const githubBaseUrl = `https://github.com/${owner}/${name}/tree/main/packages/`
+
+    const tableBody = packagesDirs
+        .map(pkg => {
+            const packageJson: PackageJson = jsonfile.readFileSync(join('packages', pkg, 'package.json'))
+            const { name, description } = packageJson
+            return `| [${name}](${githubBaseUrl}${pkg}) | ${description} | ![${name} version](https://img.shields.io/npm/v/${name}.svg?label=%20) |`
+        })
+        .join('\n')
+
+    const generatedTable = `
+## Ecosystem
+| Package | Description | Version |
+| --- | --- | --- |
+${tableBody}
+    `
+    await fs.promises.writeFile('./README.MD', readme.replace(/## Ecosystem.*(##)?/s, generatedTable), 'utf-8')
 })()
