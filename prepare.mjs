64e44b0f05f85b822408b7e2c461f10d442b8024
Vitaly Turovsky <vital2580@icloud.com>
2021-09-30 20:01:00 +0300

    dev: better crossplatform prepare script

--- prepare.mjs
+++ prepare.mjs
@@ -1,7 +1,18 @@
 //@ts-check
 import del from 'del'
 import { run } from './packages/vscode-framework/scripts/esbuild.mjs'
+import execa from 'execa'
+import * as globby from 'globby'
+
+let buildDel = await globby.globby(['**/*.tsbuildinfo', '**/build/**'])
+
+buildDel = buildDel.filter(v => v !== 'packages/vscode-framework/build/client.d.ts')
+
+await del(buildDel, {
+    // TODO! read gitignores automatically
+    // gitignore: true,
+})
+
+await execa('tsc -b tsconfig.prod.json', { preferLocal: true }).catch(e => {})
 
 await run()
-
-await del('**/*.tsbuildinfo')
