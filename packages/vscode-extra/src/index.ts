07d240389e0c144a76a4fa474b512e66a152a423
Vitaly Turovsky <vital2580@icloud.com>
2021-10-04 12:30:51 +0300

    move quickpick into separate file

--- packages/vscode-extra/src/index.ts
+++ packages/vscode-extra/src/index.ts
@@ -1,16 +1,1 @@
-import vscode from 'vscode'
-
-export type VSCodeQuickPickItem<T> = vscode.QuickPickItem & {
-    value: T
-}
-
-// TODO implement handleCancel: true
-/** Shows a selection list. {@linkcode vscode.window.showQuickPick} wrapper. */
-export const showQuickPick = async <T, M extends boolean = false>(
-    items: Array<VSCodeQuickPickItem<T>>,
-    options: vscode.QuickPickOptions & { canPickMany?: M } = {},
-): Promise<(M extends true ? T[] : T) | undefined> => {
-    const selectedItem = await vscode.window.showQuickPick(items, options)
-
-    return Array.isArray(selectedItem) ? selectedItem?.map(item => item.value) : (selectedItem?.value as any)
-}
+export * from './quickPick'
