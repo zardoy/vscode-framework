73e241737fe28325e8c0a9bae8ff23fc826a66e6
Vitaly Turovsky <vital2580@icloud.com>
2021-10-06 17:50:57 +0300

    add initial console

---
+++ packages/vscode-framework/src/cli/console-log-inject.ts
@@ -0,0 +1,6 @@
+//             outputChannel.appendLine( now.toLocaleTimeString( 'en', { hour12: false } ) + "." + String( now.getMilliseconds() ).padStart( 3, '0' ) + " " + text );
+
+
+const console = {
+    log:
+}
