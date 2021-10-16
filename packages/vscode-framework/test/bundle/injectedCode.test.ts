2cb75f03fc1f7953a44d8aca99bf51e25f9096fa
Vitaly Turovsky <vital2580@icloud.com>
2021-10-16 12:09:22 +0300

    tests: snapshot build code that is injected

---
+++ packages/vscode-framework/test/bundle/injectedCode.test.ts
@@ -0,0 +1,15 @@
+/// <reference types="jest" />
+
+import fs from 'fs'
+import { join } from 'path'
+
+// the main purpose of these tests is to see that bundler isn't misbehaving
+// with this test it's relatively safe to keep esbuild up-to-date
+// for example esbuild would remove declarations as unused etc in one of the future releases
+test('Injected console code remains the same', async () => {
+    const injectedCode = await fs.promises.readFile(
+        join(__dirname, '../../build/cli/esbuild/consoleInject.js'),
+        'utf-8',
+    )
+    expect(injectedCode).toMatchSnapshot()
+})
