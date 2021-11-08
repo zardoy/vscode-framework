99f2b5702fb3844eb34a67e448c6405c0f60113f
Vitaly Turovsky <vital2580@icloud.com>
2021-11-08 12:21:42 +0300

    ci: another ci workflow test

--- test.ts
+++ test.ts
@@ -1,3 +1,7 @@
 import fs from 'fs'
+import { join } from 'path'
 
-console.log(fs.existsSync('packages/vscode-framework/CHANGELOG.md'))
+const fromPackage = (...p: string[]) => join('packages', 'vscode-framework', ...p)
+
+console.log(process.cwd())
+console.log(await fs.promises.readFile(fromPackage('CHANGELOG.MD'), 'utf-8'))
