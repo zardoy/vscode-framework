382871e2023b9c9e10a88e2ea3f678e985bea86f
Vitaly Turovsky <vital2580@icloud.com>
2021-11-08 15:54:56 +0300

    another workflow test

--- test.ts
+++ test.ts
@@ -4,5 +4,5 @@ import { join } from 'path'
 const fromPackage = (...p: string[]) => join('packages', 'vscode-framework', ...p)
 
 console.log(process.cwd())
-console.log(process.env['$GITHUB_WORKSPACE'])
-console.log(await fs.promises.readFile(fromPackage('CHANGELOG.MD'), 'utf-8'))
+console.log(process.env['GITHUB_WORKSPACE'])
+console.log(await fs.promises.readFile('packages/vscode-framework/CHANGELOG.md', 'utf-8'))
