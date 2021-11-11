01a15076f8d6a9412674dc7912afb25e52c26979
Vitaly Turovsky <vital2580@icloud.com>
2021-11-11 23:49:03 +0300

    [skip ci] worl on integration

--- integration-tests/runTests.ts
+++ integration-tests/runTests.ts
@@ -7,16 +7,16 @@ import type { ManifestType } from '../packages/vscode-manifest/src'
 import type { MaybePromise } from '../packages/vscode-framework/src/util'
 import execa from 'execa'
 
-export const runFixtures = () => {
+export const runFixtures = async () => {
     // cwd must be root of the repository
     //@link-source-path
     if (!fs.existsSync('packages/vscode-framework/build/cli/commands.ts'))
         throw new Error('These tests require build. Run pnpm build')
-    const fromFixture = (...path: string[]) => join('fixtures', 'eslint-config')
     const binPath = 'packages/vscode-framework/bin.js'
+
     const fixturesSetup: Record<
         string,
-        (fromPath: (...path: string[]) => string, data: { fixtureName: string }) => MaybePromise<void>
+        (fromFixture: (...path: string[]) => string, data: { fixtureName: string }) => MaybePromise<void>
     > = {
         async 'eslint-config'(fromFixture, { fixtureName }) {
             // ESLint build not in framework-way, so its need additionlal config
@@ -26,10 +26,15 @@ export const runFixtures = () => {
             )
             await writeJsonFile(fromFixture('package.json'), eslintManifest as any)
             await execa('node', [binPath, 'start'], { cwd: fromFixture() })
+            const generatedFile = expect(
+                fs.promises.readFile(fromFixture('src/generated.ts'), 'utf-8'),
+            ).toMatchInlineSnapshot()
         },
     }
-    for (const fixture of fixturesSetup) {
-        const fromFixture = (...path: string[]) => join()
+
+    for (const [fixtureName, setup] of Object.entries(fixturesSetup)) {
+        const fromFixture = (...path: string[]) => join(fixtureName, ...path)
+        await setup(fromFixture, { fixtureName })
     }
 }
 
